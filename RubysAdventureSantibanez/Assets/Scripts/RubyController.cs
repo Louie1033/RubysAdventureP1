169c1eb2d451ca5df0688dfb5978ebf887878fcc
Louie1033 <louissantibanez22@gmail.com>
2022-11-14 09:57:46 -0800

    Decorated/World Interactions
    Decorated more of the landscape and worked on world interaction for ruby

--- RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
+++ RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    Rigidbody2D rigidbody2D;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-       
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
+     void FixedUpdate()
+    {
+
+        Vector2 position = rigidbody2D.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        rigidbody2D.MovePosition(position);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
     }
 }
