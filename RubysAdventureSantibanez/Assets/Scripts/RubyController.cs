51fc0562b2f64f759446d838e2dbe58f4bf1af45
Louie1033 <louissantibanez22@gmail.com>
2022-12-05 09:56:39 -0800

    Animation and Projectiles
    Finished Animation, created cog projectile and work on the scripts needed

--- RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
+++ RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
@@ -19,13 +19,17 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-
-        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        
+        currentHealth = maxHealth;        
     }
 
     void FixedUpdate()
@@ -45,11 +49,27 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
 
     }
@@ -57,14 +77,26 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
                 return;
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2D.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
